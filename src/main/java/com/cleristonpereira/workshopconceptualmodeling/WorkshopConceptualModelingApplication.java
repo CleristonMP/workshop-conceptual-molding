aed9d6ce29810b47392c48fbefedd48ede1c0100
Cleriston Pereira <cleriston.melo.pereira@gmail.com>
2021-12-08 12:56:18 -0300

    Entity Product and many-to-many association

--- src/main/java/com/cleristonpereira/workshopconceptualmodeling/WorkshopConceptualModelingApplication.java
+++ src/main/java/com/cleristonpereira/workshopconceptualmodeling/WorkshopConceptualModelingApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.cleristonpereira.workshopconceptualmodeling.domain.Category;
+import com.cleristonpereira.workshopconceptualmodeling.domain.Product;
 import com.cleristonpereira.workshopconceptualmodeling.repositories.CategoryRepository;
+import com.cleristonpereira.workshopconceptualmodeling.repositories.ProductRepository;
 
 @SpringBootApplication
 public class WorkshopConceptualModelingApplication implements CommandLineRunner {
 
 	@Autowired
-	private CategoryRepository repository;
+	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(WorkshopConceptualModelingApplication.class, args);
@@ -26,7 +31,19 @@ public class WorkshopConceptualModelingApplication implements CommandLineRunner 
 		Category cat1 = new Category(null, "Informática");
 		Category cat2 = new Category(null, "Escritório");
 		
-		repository.saveAll(Arrays.asList(cat1, cat2));
+		Product p1 = new Product(null, "Computador", 2000.00);
+		Product p2 = new Product(null, "Impressora", 800.00);
+		Product p3 = new Product(null, "Mouse", 80.00);
+		
+		cat1.getProducts().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProducts().addAll(Arrays.asList(p2));
+		
+		p1.getCategories().addAll(Arrays.asList(cat1));
+		p2.getCategories().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategories().addAll(Arrays.asList(cat1));
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
 	}
 
