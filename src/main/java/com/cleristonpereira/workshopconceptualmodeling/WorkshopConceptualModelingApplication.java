6e776ca1ed05dfd47be2408582199ab388f8aac8
Cleriston Pereira <cleriston.melo.pereira@gmail.com>
2021-12-08 12:25:50 -0300

    Creating instantiation operation

--- src/main/java/com/cleristonpereira/workshopconceptualmodeling/WorkshopConceptualModelingApplication.java
+++ src/main/java/com/cleristonpereira/workshopconceptualmodeling/WorkshopConceptualModelingApplication.java
@@ -1,13 +1,33 @@
 package com.cleristonpereira.workshopconceptualmodeling;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class WorkshopConceptualModelingApplication {
+import com.cleristonpereira.workshopconceptualmodeling.domain.Category;
+import com.cleristonpereira.workshopconceptualmodeling.repositories.CategoryRepository;
 
+@SpringBootApplication
+public class WorkshopConceptualModelingApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository repository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(WorkshopConceptualModelingApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Informática");
+		Category cat2 = new Category(null, "Escritório");
+		
+		repository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
