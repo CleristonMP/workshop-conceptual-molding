d8e7735998711ce56b887c1c11344fbd5b9bead2
Cleriston Pereira <cleriston.melo.pereira@gmail.com>
2021-12-08 10:28:55 -0300

    First commit: project created

---
+++ src/main/java/com/cleristonpereira/workshopconceptualmodeling/WorkshopConceptualModelingApplication.java
@@ -0,0 +1,13 @@
+package com.cleristonpereira.workshopconceptualmodeling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopConceptualModelingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopConceptualModelingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cleristonpereira/workshopconceptualmodeling/WorkshopConceptualModelingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleristonpereira.workshopconceptualmodeling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopConceptualModelingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
