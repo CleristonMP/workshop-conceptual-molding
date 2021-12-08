465a5904bc618751b75eeaef270ab063d5f4c365
Cleriston Pereira <cleriston.melo.pereira@gmail.com>
2021-12-08 11:19:09 -0300

    Testing the first domain class - Category

--- src/main/java/com/cleristonpereira/workshopconceptualmodeling/resources/CategoryResource.java
+++ src/main/java/com/cleristonpereira/workshopconceptualmodeling/resources/CategoryResource.java
@@ -1,16 +1,25 @@
 package com.cleristonpereira.workshopconceptualmodeling.resources;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cleristonpereira.workshopconceptualmodeling.domain.Category;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 	
 	@GetMapping
-	public String listar() {
-		return "REST funcionando!";
+	public List<Category> listar() {
+		
+		Category cat1 = new Category(1L, "Informática");
+		Category cat2 = new Category(2L, "Escritório");
+		
+		return Arrays.asList(cat1, cat2);
 	}
 
 }
